b5ed9d059017eb8d84531682c8d6a8be73698dc2
aleloria <aloria@cedesdonbosco.ed.cr>
2020-06-09 21:52:47 -0600

    Eliminación del Token

--- Sms.py
+++ Sms.py
@@ -3,7 +3,7 @@ from twilio.rest import Client
 def send_SMS(my_sms):
 
     account_sid = 'AC90e3ad16d45e359e3ec4736138798bea'
-    auth_token = 'f2a6cef38ab0df1d24249f964af2428f'
+    auth_token = ''
     client = Client(account_sid, auth_token)
 
     message = client.messages \
